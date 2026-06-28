1f86241ca9f30f69d0a1e03ded5def89ecd4e0f9
Henry Blanchette <henry@rybl.net>
2026-06-28 16:59:12 -0400

    fixed vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,21 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import packageConfig from "./package.json";
 
 export default defineConfig({
   plugins: [react()],
+
+  base: `/${packageConfig.name}/`,
+
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./vitest.setup.mjs",
+  },
+
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true,
   },
 
   resolve: {
