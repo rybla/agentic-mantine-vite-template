36d6e00949a703229431d1222847adb63b00dc19
Henry Blanchette <henry@rybl.net>
2026-06-28 17:04:30 -0400

    [checkpoint]

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,7 +3,7 @@ import { HomePage } from "@/pages/Home.page";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/agentic-mantine-vite-template",
     element: <HomePage />,
   },
 ]);
